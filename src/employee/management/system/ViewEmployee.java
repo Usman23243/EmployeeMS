9f40012ab786772cdd0170ef4083e168fab986fc
Usman23243 <usmanoffical426@gmail.com>
2025-05-23 00:29:26 -0700

    Add ViewEmployee Module

---
+++ src/employee/management/system/ViewEmployee.java
@@ -0,0 +1,111 @@
+package employee.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
+import net.proteanit.sql.DbUtils;
+import java.awt.event.*;
+
+public class ViewEmployee extends JFrame implements ActionListener {
+
+    JTable table;
+    Choice cemployeeId;
+    JButton search, print, update, back;
+
+    public ViewEmployee() {
+
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+
+        JLabel searchlbl = new JLabel("Search by Employee Id");
+        searchlbl.setBounds(20, 20, 150, 20);
+        add(searchlbl);
+
+        cemployeeId = new Choice();
+        cemployeeId.setBounds(180, 20, 150, 20);
+        add(cemployeeId);
+
+        try {
+            Cone c = new Cone();
+            ResultSet rs = c.s.executeQuery("select * from employee");
+            while (rs.next()) {
+                cemployeeId.add(rs.getString("empId"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        table = new JTable();
+
+        try {
+            Cone c = new Cone();
+            ResultSet rs = c.s.executeQuery("select * from employee");
+            table.setModel(DbUtils.resultSetToTableModel(rs));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        JScrollPane jsp = new JScrollPane(table);
+        jsp.setBounds(0, 100, 900, 600);
+        add(jsp);
+
+        search = new JButton("Search");
+        search.setBounds(20, 70, 80, 20);
+        search.addActionListener(this);
+        add(search);
+
+        print = new JButton("Print");
+        print.setBounds(120, 70, 80, 20);
+        print.addActionListener(this);
+        add(print);
+
+        update = new JButton("Update");
+        update.setBounds(220, 70, 80, 20);
+        update.addActionListener(this);
+        add(update);
+
+        back = new JButton("Back");
+        back.setBounds(320, 70, 80, 20);
+        back.addActionListener(this);
+        add(back);
+
+        setSize(900, 700);
+        setLocation(300, 100);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+if (ae.getSource() == search) {
+    String query = "SELECT * FROM employee WHERE empId = ?";
+    try {
+        Cone c = new Cone();
+        PreparedStatement pst = c.c.prepareStatement(query); // note c.c is your Connection
+        pst.setString(1, cemployeeId.getSelectedItem()); // set the empId parameter
+        ResultSet rs = pst.executeQuery();
+        table.setModel(DbUtils.resultSetToTableModel(rs));
+        pst.close();
+        c.c.close();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+else if (ae.getSource() == print) {
+            try {
+                table.print();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (ae.getSource() == update) {
+            setVisible(false);
+            new UpdateEmployee(cemployeeId.getSelectedItem());
+        } else {
+            setVisible(false);
+            new Home();
+        }
+    }
+
+    public static void main(String[] args) {
+        new ViewEmployee();
+    }
+}
